d9e254b47aa35792de482725d0b5480d5b52f060
Arie Twigt <ariekanarie@twigtd4.nl>
2022-11-10 12:06:53 +0100

    minor changes to join example

--- 22_join_brand_country.sql
+++ 22_join_brand_country.sql
@@ -1,7 +1,8 @@
 -- SQLite
-SELECT cars.brand, 
-       cars.model,
-       brands.country
+SELECT brands.country,
+       AVG(cars.price) AS total_amount
 FROM registered_cars AS cars
 JOIN car_brands AS brands
   ON cars.brand = brands.brand
+GROUP BY 1
+ORDER BY total_amount DESC
