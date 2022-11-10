a51c03536f251a0012032bc7142aef9175b83b81
Arie Twigt <ariekanarie@twigtd4.nl>
2022-11-10 11:59:44 +0100

    added first example for join

--- assignment_4_6.sql
+++ assignment_4_6.sql
@@ -3,10 +3,11 @@ SELECT
        cars.brand, 
        cars.model,
        cars.color,
-       MIN(cars.price) min_price,
-       MAX(cars.price) max_price,
-       AVG(cars.price) avg_price
+       MIN(cars.price) AS min_price,
+       MAX(cars.price) AS max_price,
+       AVG(cars.price) AS avg_price
 FROM registered_cars AS cars
 WHERE cars.brand = 'VOLKSWAGEN'
-  AND cars.model = 'GOLF'
+  AND cars.model LIKE 'GOLF%'
 GROUP BY 1,2,3
+ORDER BY avg_price DESC
