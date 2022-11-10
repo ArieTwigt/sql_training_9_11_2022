be0eb628450c26abce20dc31919c568b9defb477
Arie Twigt <ariekanarie@twigtd4.nl>
2022-11-10 15:30:07 +0100

    improved 5_2

--- assignment_5_2.sql
+++ assignment_5_2.sql
@@ -1,23 +1,22 @@
-WITH cars_hybrid AS (
-    
-    SELECT cars.brand,
-                      cars.model,
-                      cars.plate,
-                      AVG(cars.price) AS average_price,
-                      SUM(motor.net_power_fuel) AS power_fuel,
-                      SUM(motor.net_power_electric) AS power_electric
-    FROM cars_motor AS motor
-    JOIN registered_cars AS cars
-      ON motor.plate = cars.plate
-    WHERE cars.brand IN ('VOLVO', 'LEXUS')
-    GROUP BY 1,2
-    HAVING power_electric IS NOT NULL
-       AND power_fuel IS NOT NULL
+WITH hybrid_cars AS (
 
-)    
+  SELECT cars.plate,
+                     cars.brand,
+                     cars.model,
+                     AVG(cars.price) AS avg_price,
+                     SUM(motor.net_power_fuel) AS sum_fuel,
+                     SUM(motor.net_power_electric) AS sum_electric
+              FROM cars_motor AS motor
+              JOIN registered_cars AS cars
+                ON motor.plate = cars.plate
+              WHERE cars.brand IN ('VOLVO', 'LEXUS')
+              GROUP BY 1,2,3
+              HAVING sum_fuel IS NOT NULL
+                 AND sum_electric IS NOT NULL
+) 
 
-SELECT cars_hybrid.brand,
-       ROUND(AVG(cars_hybrid.average_price), 2) AS avg_price
-FROM cars_hybrid   
+SELECT hybrid_cars.brand,
+       ROUND(AVG(hybrid_cars.avg_price), 2) AS avg_price
+FROM hybrid_cars
 GROUP BY 1
-ORDER BY avg_price DESC      
+ORDER BY 2 DESC
